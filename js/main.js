8c6143915c4c8c861cf5cc71a46b3ece939582ac
BuildTools <unconfigured@null.spigotmc.org>
2020-12-18 11:30:43 +0100

    ajout footer + boutons + page 1 & 2 + transition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-
-$(document).ready(function (){
-    //$("li:eq(0)").hide();
+$(document).ready(function() {
+    $("li:eq(0)").on("click",function (){
+        $.mobile.changePage("page1.html",{
+            transition: "slide",
+        });
+    });
+    $("li:eq(1)").on("click",function (){
+        $.mobile.changePage("page2.html",{
+            transition: "slide",
+        });
+    });
+    styles = { 'height': '20px', 'font-size': '20px' };
+    $('.ui-btn').css(styles);
+    $(document).on("pagechange",function() {
+        styles = { 'height': '20px', 'font-size': '20px' };
+        $('.ui-btn').css(styles);
+    });
 });
